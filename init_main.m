7a2433c825c5f6580b7a1fc7accdcd8c3e00c9d9
AlessandroZavoli <alessandro.zavoli@uniroma1.it>
2020-04-23 23:12:23 +0200

    Release 0

---
+++ init_main.m
@@ -0,0 +1,5 @@
+%Carica le definizioni dei bus
+load('bus_stato6DoF.mat')
+
+%carica dati del motore
+init_motore
---
+++ init_motore.m
@@ -0,0 +1,4 @@
+% function init_motore
+% inizializza i dati motore
+
+motorData.C_Q_coeff = 1.
